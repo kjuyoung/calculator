82bb91a244a0824abc5ca0e31fce1a6c793f331d
charleswayo <charleswayo@gmail.com>
2024-02-20 21:44:26 +0900

    fix: Calculator 클래스에 operand 형식 체크 메소드 추가

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -7,10 +7,12 @@ import operation.Operation;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
     private final Map<String, Operation> operations = new HashMap<>();
+    private final Pattern operandPattern = Pattern.compile("[0-9]+[.]?[0-9]*");
 
     public Calculator() {
         operations.put("+", new Addition());
@@ -19,7 +21,16 @@ public class Calculator {
         operations.put("/", new Division());
     }
 
+    public boolean isValidOperand(String operand) {
+        return !operandPattern.matcher(operand).matches();
+    }
+
     public BigDecimal calculate(BigDecimal operand1, String operation, BigDecimal operand2) {
+
+        if (isValidOperand(operand1.toPlainString()) || isValidOperand(operand2.toPlainString())) {
+            throw new IllegalArgumentException("Invalid operand");
+        }
+
         Operation op = operations.get(operation);
         if (op == null) {
             throw new IllegalArgumentException("Unknown operation");
