2d2e20c5633b76cd2b6739fa3c683ccac599aca6
charleswayo <charleswayo@gmail.com>
2024-02-20 21:13:20 +0900

    fix: main 메소드 수정

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,5 +1,23 @@
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private final Map<String, Operation> operations = new HashMap<>();
+
+    public Calculator() {
+        operations.put("+", new Addition());
+        operations.put("-", new Subtraction());
+        operations.put("*", new Multiplication());
+        operations.put("/", new Division());
+    }
+
+    public BigDecimal calculate(BigDecimal operand1, String operation, BigDecimal operand2) {
+        Operation op = operations.get(operation);
+        if (op == null) {
+            throw new IllegalArgumentException("Unknown operation");
+        }
+        return op.calculate(operand1, operand2);
     }
 }
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter operand1, operation (+, -, *, /), operand2 : ");
+        String[] inputs = scanner.nextLine().split(" ");
+
+        BigDecimal operand1 = new BigDecimal(inputs[0]);
+        BigDecimal operand2 = new BigDecimal(inputs[2]);
+
+        String operation = inputs[1].trim();
+
+        BigDecimal result = calculator.calculate(operand1, operation, operand2);
+        System.out.println(operand1 + " " + operation + " " + operand2 + " = " + result);
+    }
+}
